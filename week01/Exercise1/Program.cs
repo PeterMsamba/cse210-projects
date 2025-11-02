3b3a4ce9ee455c1f929e1e064d98da85378320c3
petermsamba <petermsamba95@gmail.com>
2025-11-03 00:52:33 +0200

    exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,24 @@
 using System;
-
+/*
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+    }
+}
+*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your surname? ");
+        string surname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {surname}, {firstName} {surname}.");
     }
 }
