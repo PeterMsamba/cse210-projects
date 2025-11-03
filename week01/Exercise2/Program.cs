3e3ceb05f86bdb5e3639a6525d19646c1ff3dc76
petermsamba <petermsamba95@gmail.com>
2025-11-03 04:54:30 +0200

    working on exarcise two

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your percentage grade? ");
+        string percentage_grade = Console.ReadLine();
+        int grade = Convert.ToInt32(percentage_grade);
+
+        string letter; 
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        // print statement for the letter grade
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass this time. Keep your head up and good luck next time!");
+        }
     }
 }
