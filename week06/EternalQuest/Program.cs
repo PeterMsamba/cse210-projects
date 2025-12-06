4acd9c200f0ad2adc1557813858de058c84bcc30
petermsamba <pmsamba@byupathway.edu>
2025-12-06 22:22:44 +0200

    editting

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,5 +1,7 @@
 using System;
-
+//creativity is Add your own ideas for gamification. 
+//This could include leveling up, earning certain bonuses, 
+//or other "fun" aspects to the quest.
 class Program
 {
     static void Main(string[] args)
